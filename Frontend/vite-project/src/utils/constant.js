579c54f775e0f2935daa0d58b5806dc543d00170
Piyush  Singh <piyushkumar120011@gmail.com>
2026-02-13 20:35:30 +0530

    add new changes

--- Frontend/vite-project/src/utils/constant.js
+++ Frontend/vite-project/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const USER_API_END_POINT = `${BASE}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE}/api/v1/job`;
